ae98ce4af4c959c219a9c5900a342bf757d8cc1d
tfb34 <britothalia212@gmail.com>
2017-05-07 13:37:42 -0400

    adding a user_id column to posts to create association

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170507012543) do
+ActiveRecord::Schema.define(version: 20170507170443) do
 
   create_table "posts", force: :cascade do |t|
     t.string   "link"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer  "user_id"
+    t.index ["user_id"], name: "index_posts_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
